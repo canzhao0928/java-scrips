fd12f20cb2a4461e5028ca8ee648d01e39695e2b
Can Zhao <zhaocan0928@gmail.com>
2022-08-17 19:42:40 +1000

    update pig game styles.

--- guess my number/script.js
+++ guess my number/script.js
@@ -3,29 +3,31 @@ let secretNum = 18
 let score = 20
 
 document.querySelector(".check").addEventListener("click", function () {
-    const guess = Number(document.querySelector(".guess").value);
-    if (!guess) {
-        document.querySelector(".message").textContent = "🙅🏻‍♀️ no number";
-    } else if (guess > 20 || guess < 1) {
-        document.querySelector(".message").textContent = "Number should be between 1 and 20";
-    } else if (guess !== secretNum) {
-        if (score > 1) {
-            document.querySelector(".message").textContent = guess > secretNum ? "📈 Too high!" : "📉 Too low!";
-            score--;
+    if (document.querySelector(".message").textContent !== "🎊 Correct! You WIN! 🎊") {
+        const guess = Number(document.querySelector(".guess").value);
+        if (!guess) {
+            document.querySelector(".message").textContent = "🙅🏻‍♀️ no number";
+        } else if (guess > 20 || guess < 1) {
+            document.querySelector(".message").textContent = "Number should be between 1 and 20";
+        } else if (guess !== secretNum) {
+            if (score > 1) {
+                document.querySelector(".message").textContent = guess > secretNum ? "📈 Too high!" : "📉 Too low!";
+                score--;
+                document.querySelector(".score").textContent = score;
+            } else {
+                document.querySelector(".message").textContent = "You lose..."
+                document.querySelector(".score").textContent = 0;
+            }
+        } else if ((guess === secretNum)) {
+            document.querySelector(".message").textContent = "🎊 Correct! You WIN! 🎊";
+            document.querySelector(".number").textContent = guess;
+            score++;
             document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "You lose..."
-            document.querySelector(".score").textContent = 0;
-        }
-    } else if ((guess === secretNum)) {
-        document.querySelector(".message").textContent = "🎊 Correct! You WIN! 🎊";
-        document.querySelector(".number").textContent = guess;
-        score++;
-        document.querySelector(".score").textContent = score;
-        document.querySelector("body").style.backgroundColor = "#60b347";
-        document.querySelector(".number").style.width = "30rem";
-        if (score > document.querySelector(".highscore").textContent) {
-            document.querySelector(".highscore").textContent = score;
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").style.width = "30rem";
+            if (score > document.querySelector(".highscore").textContent) {
+                document.querySelector(".highscore").textContent = score;
+            }
         }
     }
 });
--- pig game/script.js
+++ pig game/script.js
@@ -1,58 +1,69 @@
 'use strict';
 
 let currenPlayer = 0;
+let isPlay = true;
 
-//caculate players score in one round
-function updateScore(currenPlayer) {
-
-    let playerScore = Number(document.querySelector(`#score--${currenPlayer}`).textContent);
-    playerScore += Number(document.querySelector(`#current--${currenPlayer}`).textContent);
-    if (playerScore >= 100) {
-        document.querySelector(`#score--${currenPlayer}`).textContent = "🎊 WIN 🎊";
-    } else {
-        document.querySelector(`#score--${currenPlayer}`).textContent = playerScore;
-    }
+function changPlayer() {
     document.querySelector(`#current--${currenPlayer}`).textContent = 0;
-
+    currenPlayer = currenPlayer === 0 ? 1 : 0;
+    document.querySelector(".player--0").classList.toggle('player--active');
+    document.querySelector(".player--1").classList.toggle('player--active');
 }
 
+// init data
+function init() {
+    currenPlayer = 0;
+    document.querySelector("#current--0").textContent = 0;
+    document.querySelector("#current--1").textContent = 0;
+    document.querySelector("#score--0").textContent = 0;
+    document.querySelector("#score--1").textContent = 0;
+    document.querySelector(".player--0").classList.remove("player--active");
+    document.querySelector(".player--1").classList.add("player--active");
+    document.querySelector(".player--0").classList.remove("player--winner");
+    document.querySelector(".player--1").classList.remove("player--winner");
+    isPlay = true;
+
+}
+init();
 
 //roll dice to generate a number and update the img.
 document.querySelector(".btn--roll").addEventListener("click", function () {
-    const num = Math.floor(Math.random() * 6) + 1;
-    console.log(num);
-    document.querySelector(".dice").setAttribute("src", 'dice-' + num + '.png');
-    if (num === 1) {
-        //clear current score
-        currenPlayer === 0 ? document.querySelector("#current--0").textContent = 0 : document.querySelector("#current--1").textContent = 0;
-        //change player by number
-        currenPlayer = currenPlayer === 0 ? 1 : 0;
-        document.querySelector("#current--0").classList.toggle('player--active');
-        document.querySelector("#current--1").classList.toggle('player--active');
-    } else {
-        //add to current score
-        let playerCurrentScore = Number(document.querySelector(`#current--${currenPlayer}`).textContent) + num;
-        document.querySelector(`#current--${currenPlayer}`).textContent = playerCurrentScore;
+    if (isPlay) {
+        const num = Math.floor(Math.random() * 6) + 1;
+        document.querySelector(".dice").setAttribute("src", 'dice-' + num + '.png');
+        if (num === 1) {
+            //clear current score and change player
+            changPlayer();
+        } else {
+            //add to current score
+            let playerCurrentScore = Number(document.querySelector(`#current--${currenPlayer}`).textContent) + num;
+            document.querySelector(`#current--${currenPlayer}`).textContent = playerCurrentScore;
+        }
     }
 
 })
 
 //change player hold and update score
 document.querySelector(".btn--hold").addEventListener("click", function () {
-    //update scores
-    updateScore(currenPlayer);
-    //change player
-    currenPlayer = currenPlayer === 0 ? 1 : 0;
-    document.querySelector("#current--0").classList.toggle('player--active');
-    document.querySelector("#current--1").classList.toggle('player--active');
+    if (isPlay) {
+        //update scores
+        let playerScore = Number(document.querySelector(`#score--${currenPlayer}`).textContent);
+        playerScore += Number(document.querySelector(`#current--${currenPlayer}`).textContent);
+        if (playerScore >= 10) {
+            document.querySelector(`#score--${currenPlayer}`).textContent = "🎉  WIN";
+            document.querySelector(`.player--${currenPlayer}`).classList.add("player--winner");
+            document.querySelector(`.player--${currenPlayer}`).classList.remove("player--active");
+            isPlay = false;
+        } else {
+            document.querySelector(`#score--${currenPlayer}`).textContent = playerScore;
+        }
+        //change player
+        changPlayer();
+    }
 })
 
 
 document.querySelector(".btn--new").addEventListener("click", function () {
-    document.querySelector("#current--0").textContent = 0;
-    document.querySelector("#current--1").textContent = 0;
-    document.querySelector("#score--0").textContent = 0;
-    document.querySelector("#score--1").textContent = 0;
-    document.querySelector("#current--1").classList.remove("player--active");
-    currenPlayer = 0;
+    init();
 })
+
