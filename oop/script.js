2246d7086f2446405715677e6d7838fc1ca3ed34
Can Zhao <zhaocan0928@gmail.com>
2022-09-07 13:41:24 +1000

    add challenge3

--- oop/script.js
+++ oop/script.js
@@ -2,7 +2,7 @@
 //////////////////////////////////////
 // Coding Challenge #1
 
-/* 
+/*
 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
@@ -31,7 +31,7 @@ GOOD LUCK 😀
 ///////////////////////////////////////
 // Coding Challenge #2
 
-/* 
+/*
 1. Re-create challenge 1, but this time using an ES6 class;
 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
@@ -59,3 +59,70 @@ class Car {
 const car1 = new Car("Ford", 120)
 car1.speedUS = 20
 console.log(car1.speed);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+//constructor function
+// const EV = function (make, speed, charge) {
+//     Car.call(this, make, speed);
+//     this.charge = charge;
+// }
+
+
+
+// EV.prototype = Object.create(Car.prototype)
+// EV.prototype.chargeBattery = function (chargeTo) {
+//     this.charge = chargeTo
+// }
+
+// EV.prototype.accelerate = function () {
+//     this.speed += 20;
+//     this.charge *= 0.99;
+//     console.log(`Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+// }
+
+// const tesla = new EV("Tesla", 120, 23)
+
+// console.log(tesla);
+// tesla.accelerate()
+// tesla.brake()
+// tesla.chargeBattery(90)
+// tesla.accelerate()
+
+//class
+class EV extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.charge = chargeTo
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.charge *= 0.99;
+        console.log(`Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+    }
+}
+
+const tesla = new EV("Tesla", 120, 23)
+
+console.log(tesla);
+tesla.accelerate()
+tesla.brake()
+tesla.chargeBattery(90)
+tesla.accelerate()
